ff201a04d8f2057e6a33239d04d75d0c6545430c
Sean Quillen <sean.r.quillen@gmail.com>
2021-01-29 10:33:55 -0600

    sidebar working

--- src/Header.js
+++ src/Header.js
@@ -11,13 +11,13 @@ export default function Header() {
         </button>
       </form>
       <nav className="user-nav">
-        <div className="user-nav__nav__icon-box">
+        <div className="user-nav__icon-box">
           <svg className="user-nav__icon">
             <use xlinkHref="img/sprite.svg#icon-bookmark" />
           </svg>
           <span className="user-nav__notification">7</span>
         </div>
-        <div className="user-nav__nav__icon-box">
+        <div className="user-nav__icon-box">
           <svg className="user-nav__icon">
             <use xlinkHref="img/sprite.svg#icon-chat" />
           </svg>
